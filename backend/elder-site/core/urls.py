0ab96c7112950f4729d4e2d95e34aecfeed4aef8
kzhao132018@gmail.com <kzhao132018@gmail.com>
2026-01-11 01:39:06 -0800

    views update

--- backend/elder-site/core/urls.py
+++ backend/elder-site/core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import healthcheck, create_person, upload_footage, list_people, get_all_individual_videos, footage_thumbnail
+from .views import healthcheck, create_person, upload_footage, list_people, get_all_individual_videos, footage_thumbnail, serve_video
 
 urlpatterns = [
     path("health/", healthcheck),
@@ -10,6 +10,7 @@ urlpatterns = [
     path("api/footage/upload/", upload_footage, name="upload_footage"),
     path("api/footage/all/", get_all_individual_videos, name="get_all_individual_videos"),
     path("api/footage/thumb/<int:video_id>/", footage_thumbnail, name="footage_thumbnail"),
+    path("api/footage/video/<int:video_id>/", serve_video, name="serve_video"),
 
 
     # 👇 ADD THIS LINE
